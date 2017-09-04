3f3012675f8e61b3dcaac74c7087b10546895dcb
umanari145 <umanari145@gmail.com>
2017-09-04 20:03:22 +0900

    vue.jsの簡単なサンプルを実装(変数とメソッドの簡単な実装)

---
+++ vue.js
@@ -0,0 +1,52 @@
+window.onload = function() {
+
+	var demo = new Vue({
+		el : '#demo',
+		data : {
+			message : 'Hello world'
+		}
+	})
+
+	var app = new Vue({
+		el : '#app',
+		data : {
+			todos : [ {
+				text : 'homerun'
+			}, {
+				text : 'two base'
+			}, {
+				text : 'three base'
+			} ],
+			message :'ok'
+		},
+		methods:{
+			showMessage : function(){
+				//スコープに注意!
+				//functionの外の引数は取れない
+				console.log(this.message);
+			}
+		}
+	})
+
+	var app = new Vue({
+		el : '#app2',
+		data : {
+			todos : [ {
+				text : 'homerun'
+			}]
+		},
+		methods:{
+			add : function(){
+				var text = this.input.trim();
+				if (text) {
+					this.todos.push({text:text});
+				}
+			},
+			remove:function(rowIndex){
+				//indexの配列の位置を1つだけ削除
+				this.todos.splice(rowIndex,1)
+			}
+		}
+	})
+
+}
