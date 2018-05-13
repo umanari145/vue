eb729f10310f017c1d68877a4a5968957048b4d7
Norio <umanari145@gmail.com>
2018-05-13 21:31:18 +0900

    boootstrapをnpmで

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+
+var gulp = require('gulp');
+var webpack = require('gulp-webpack');
+var config = require('./config.js');
+var gulp_sass = require('gulp-sass');
+
+
+gulp.task('default',['css']);
+
+gulp.task('webpack',function(){
+    //js
+    return gulp.src(config.webpack.entry)
+    .pipe(webpack(config.webpack))
+    .pipe(gulp.dest(config.js.dest));
+});
+
+
+gulp.task('css',function(){
+    //css
+    return gulp.src(config.css.src)
+           .pipe(gulp_sass())
+           .pipe(gulp.dest(config.css.dest));
+});
