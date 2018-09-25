b5fbb239072e7a315e597f93ac10b7cc8b5016ab
Norio <umanari145@gmail.com>
2018-09-25 23:11:08 +0900

    vueをnpmで読み込み実処理は外部のjsで書く

---
+++ resource4.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import VueResource from 'vue-resource'
+
+Vue.use(VueResource)
+window.Vue = Vue
+window.Sugar = require('Sugar')
---
+++ vue4.js
@@ -0,0 +1,58 @@
+window.onload = function() {
+
+	var link = 'http://localhost/vue/kenapi.py?area_type=pref'
+	var citylink = 'http://localhost/vue/kenapi.py?area_type=city&pref='
+	var townlink = 'http://localhost/vue/kenapi.py?area_type=town&pref='
+
+	var app = new Vue({
+		el : '#app',
+		data : {
+			prefs:[],
+			selectedPref:'',
+			isshowpref:1,
+			cities:[],
+			selectedCities:[],
+			isshowcity:0,
+			towns:[],
+			selectedTowns:[],
+			isshowtown:0,
+			groupCities:[]
+		},
+		methods:{
+			checkPref : function(){
+				var selectedPref = Sugar.String(this.selectedPref)
+				if (!selectedPref.isBlank().raw ){
+					citylink2 = citylink + selectedPref.raw
+					this.$http.get(citylink2).then(function(response){
+						this.cities = response.data;
+						this.isshowpref = 0
+						this.isshowcity = 1
+					}, function(error){
+						console.log(error.statusText);
+					});
+				}
+			},
+			checkTown : function(){
+				if (this.selectedCities.length >0){
+					var cityStr = this.selectedCities.join(",")
+					townlink2 = townlink + this.selectedPref + '&city=' + cityStr
+					this.$http.get(townlink2).then(function(response){
+						townsRes = response.data;
+						this.groupCities = Sugar.Array(townsRes).groupBy('city').raw
+						this.isshowcity = 0
+						this.isshowtown = 1
+					}, function(error){
+						console.log(error.statusText);
+					});
+				}
+			}
+		},
+		created:function(){
+			this.$http.get(link).then(function(response){
+				this.prefs = response.data;
+			}, function(error){
+				console.log(error.statusText);
+			});
+		}
+	})
+}
