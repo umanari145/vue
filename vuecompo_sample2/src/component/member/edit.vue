2645aae26b9ea69ef46e85723208f6ad9e20b6a7
Norio <umanari145@gmail.com>
2019-06-16 22:52:34 +0900

    編集画面を追加

---
+++ vuecompo_sample2/src/component/member/edit.vue
@@ -0,0 +1,2 @@
+<template src="./edit.htm"></template>
+<script scoped src="./edit.js">
--- vuecompo_sample2/src/router.js
+++ vuecompo_sample2/src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 import regist from './component/member/regist.vue'
+import edit from './component/member/edit.vue'
 import list from './component/member/list.vue'
 
 
@@ -16,6 +17,10 @@ const router = new VueRouter({
         {
             path:'/regist',
             component:regist
+        },
+        {
+            path:'/edit/:hash_key',
+            component:edit
         }
     ]
 })
