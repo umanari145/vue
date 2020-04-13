b4c2a1cadfb3a91d66a5f2ea752bc37d1dc910f4
Norio <umanari145@gmail.com>
2020-04-13 21:35:06 +0900

    地域もーだる

--- areaModal.php
+++ areaModal.php
@@ -4,9 +4,26 @@
 :scrollable=true
 @before-open="getPref"
 >
+    <div class="areaWrapper">
 <!--ローディング-->
-<div class="spininng_back" v-if="is_show_spinner == 1">
-    <b-spinner label="Spinning" type="grow">
-    </b-spinner>
-</div>
+        <div class="spininng_back" v-if="is_show_spinner == 1">
+            <b-spinner label="Spinning" type="grow">
+            </b-spinner>
+        </div>
+
+        <div class="pref_area">
+            <li v-for="(pref) in prefs" style="display:inline-block;">
+                <label class="pref_label" :for="`pref_${pref.prefCode}`">
+                <input type="checkbox"
+                    :id = "`pref_${pref.prefCode}`"
+                    v-model="selected_prefs[target_index]"
+                    :value="pref.prefCode"
+                >
+                {{pref.prefName}}
+                </label>
+            </li>
+            <b-button variant="outline-primary" @click="selectPref()">選択</b-button>
+            <b-button variant="outline-primary" @click="">市区</b-button>
+        </div>
+    </div>
 </modal>
