4fc48daefc24a8f0535523acdae27f263c514603
Norio <umanari145@gmail.com>
2020-04-13 21:34:40 +0900

    希望地域の選択

--- customer.js
+++ customer.js
@@ -8,34 +8,61 @@ $(function(){
 		el : '#app',
 		data : {
 			hope_regident_list:[],
-			is_show_modal:false
+			is_show_modal:false,
+			is_show_spinner:0,
+			prefs:[],
+			selected_prefs:[],
+			selected_prefs_str:[],
+			selected_prefs_str_concat:[],
+			target_index:'',
+			master_pref_hash:{},
+
 		},
 		methods:{
 			bootModal(i) {
+				console.log(i);
+				this.target_index = i;
 				this.$modal.show('area_modal');
 			},
 			getPref() {
+				this.is_show_spinner = 1;
 				$.ajax({
-					url:this.apiUrl + 'prefectures',
+					url:apiUrl + 'prefectures',
 					type:'GET',
 					headers:{
-						'X-API-KEY':this.apiKey
+						'X-API-KEY':apiKey
 					}
 				}).done((data) => {
-					let pref_html = '';
-					if (data['result'] !== undefined && data['result'].length > 0) {
-						let pref_html = this.loadHtml(data,'pref', 6)
-						$('#pref_table').html(pref_html)
+					if (data['result'] !== null && data['result'].length > 0) {
+						this.prefs = data['result'];
+						for(var i=0; i < data['result'].length; i++) {
+							let prefCode = data['result'][i]['prefCode'];
+							let prefName = data['result'][i]['prefName'];
+							this.master_pref_hash[prefCode] = prefName;
+						}
 					}
 				}).fail((data) => {
 					alert("サーバーとの通信に失敗しました。")
 				}).always(()=>{
-					
+					this.is_show_spinner = 0;
 				})
+			},
+			selectPref() {
+				this.selected_prefs_str[this.target_index] = [];
+				for(var i = 0 ;i < this.selected_prefs[this.target_index].length; i++) {
+					let prefCode = this.selected_prefs[this.target_index][i];
+					let prefName = this.master_pref_hash[prefCode];
+					this.selected_prefs_str[this.target_index].push(prefName)
+				}
+				Vue.set(this.selected_prefs_str_concat, this.target_index, this.selected_prefs_str[this.target_index].join(','));
 			}
 		},
 		created:function(){
-			this.constant_reports = []
+			for(var i = 1; i <= 4; i++) {
+				this.selected_prefs[i] =[];
+				this.selected_prefs_str[i] =[];
+				this.selected_prefs_str_concat[i] = "";
+			}
 		},
 		mounted:function() {
 
--- customer.php
+++ customer.php
@@ -12,14 +12,33 @@
  </script>
 <script src="https://unpkg.com/vue"></script>
 <script src="https://cdn.jsdelivr.net/npm/vue-js-modal@1.3.28/dist/index.min.js"></script>
+<script src="https://unpkg.com/babel-polyfill@latest/dist/polyfill.min.js"></script>
+<script src="https://unpkg.com/bootstrap-vue@latest/dist/bootstrap-vue.js"></script>
+
+<link type="text/css" rel="stylesheet" href="https://unpkg.com/bootstrap/dist/css/bootstrap.min.css" />
+<link type="text/css" rel="stylesheet" href="https://unpkg.com/bootstrap-vue@latest/dist/bootstrap-vue.css" />
+
 <script src="./vue_common.js"></script>
+<script src="./areaModalConfig.js"></script>
 <script src="./customer.js"></script>
+<style type="text/css">
+[v-cloak] {
+  display: none;
+}
+li{
+    list-style-type: none;
+}
+.pref_label{
+    width: 200px;
+}
+</style>
 <body>
 	<div id="app" v-cloak>
         <ul>
             <li v-for="i in 4" :key="i">
                 希望勤務地{{i}}
                 <button @click="bootModal(i)">地域を追加</button>
+                {{selected_prefs_str_concat[`${i}`]}}
             </li>
         </ul>
         <?php include_once('areaModal.php');?>
